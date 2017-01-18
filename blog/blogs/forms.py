68138ad6521a9549e17080968d7ec2d145d9639a
Aaditya Agrawal <aaditya.agrawal@cuelogic.com>
2017-01-18 17:09:30 +0530

    Removed comments

--- blog/blogs/forms.py
+++ blog/blogs/forms.py
@@ -7,31 +7,43 @@ from django.utils.translation import ugettext_lazy as _
 # If you don't do this you cannot use Bootstrap CSS
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Username", max_length=30,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+                    widget=forms.TextInput(attrs={'class': 'form-control',
+                    'name': 'username'}))
     password = forms.CharField(label="Password", max_length=30,
-                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
+                    widget=forms.PasswordInput(
+                        attrs={'class': 'form-control', 'name': 'password'}))
 
 
 class RegistrationForm(forms.Form):
 
-    username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
-    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
+    username = forms.RegexField(regex=r'^\w+$',
+        widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
+        label=_("Username"), error_messages={ 'invalid':
+        _("This value must contain only letters, numbers and underscores.") })
+    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True,
+        max_length=30)), label=_("Email address"))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs=
+        dict(required=True, max_length=30, render_value=False)),
+         label=_("Password"))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs=
+        dict(required=True, max_length=30, render_value=False)),
+         label=_("Password (again)"))
 
     def clean_username(self):
         try:
             user = User.objects.get(username__iexact=self.cleaned_data['username'])
         except User.DoesNotExist:
             return self.cleaned_data['username']
-        raise forms.ValidationError(_("The username already exists. Please try another one."))
+        raise forms.ValidationError(
+            _("The username already exists. Please try another one."))
 
     def clean_email(self):
         try:
             user = User.objects.get(email__iexact=self.cleaned_data['email'])
         except User.DoesNotExist:
             return self.cleaned_data['email']
-        raise forms.ValidationError(_("The email already exists. Please try another one."))
+        raise forms.ValidationError(
+            _("The email already exists. Please try another one."))
 
     def clean(self):
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
