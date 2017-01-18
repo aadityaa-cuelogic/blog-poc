3f28538d041bf7cc1a0caa3dc6c64ef1465c251e
Aaditya Agrawal <aaditya.agrawal@cuelogic.com>
2017-01-18 17:03:28 +0530

    Removed comments

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -24,16 +24,26 @@ from blogs.forms import LoginForm, RegistrationForm
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blogs.urls')),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name="login"),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html',
+        'authentication_form': LoginForm}, name="login"),
     url(r'^logout/$', auth_views.logout, {'next_page': '/login'}),
-    url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'forgot_password.html'}, name="forgot_password"),
-    url(r'^password_reset/done$', auth_views.password_reset_done,{'template_name': 'forgot_password_done.html'}, name='password_reset_done'),
+    url(r'^password_reset/$', auth_views.password_reset,
+        {'template_name': 'forgot_password.html'}, name="forgot_password"),
+    url(r'^password_reset/done$', auth_views.password_reset_done,{
+        'template_name':'forgot_password_done.html'}, name='password_reset_done'),
 
-    url(r'^password_change/$', auth_views.password_change, {'template_name': 'password_change.html'}, name="password_change"),
-    url(r'^password_change/done$', auth_views.password_change_done,{'template_name': 'password_change_done.html'}, name='password_change_done'),
+    url(r'^password_change/$', auth_views.password_change,
+        {'template_name':'password_change.html'}, name="password_change"),
+    url(r'^password_change/done$', auth_views.password_change_done,
+        {'template_name': 'password_change_done.html'}, name='password_change_done'),
 
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, {'template_name': 'password_reset_confirm.html'}, name="password_reset_confirm"),
-    url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'password_reset_complete.html'}, name="password_reset_complete"),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm,
+        {'template_name': 'password_reset_confirm.html'},
+        name="password_reset_confirm"),
+    url(r'^reset/done/$', auth_views.password_reset_complete,
+        {'template_name': 'password_reset_complete.html'},
+        name="password_reset_complete"),
 
     url(r'^register/$', views.register, name="register"),
     url(r'^register/success/$', views.register_success,),
--- blog/blogs/views.py
+++ blog/blogs/views.py
@@ -30,20 +30,6 @@ def register(request):
     else:
         context = {'form':RegistrationForm() }
         return render(request, 'register.html', context)
-    # if request.method == 'POST':
-    #     form = RegistrationForm(request.POST)
-    #     if form.is_valid():
-    #         user = User.objects.create_user(
-    #             username=form.cleaned_data['username'],
-    #             password=form.cleaned_data['password1'],
-    #             email=form.cleaned_data['email']
-    #         )
-    #         return HttpResponseRedirect('/register/success/')
-    # else:
-    #     form = RegistrationForm()
-    #
-    # variables = RequestContext(request, {'form': form })
-    # return render_to_response('register.html', variables,)
 
 def register_success(request):
     return render_to_response('register_success.html', )
