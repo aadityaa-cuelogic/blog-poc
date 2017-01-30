dbd32a1303ece784c6c8628657af12e2f7986d26
Aaditya Agrawal <aaditya.agrawal@cuelogic.com>
2017-01-30 12:02:35 +0530

    Commented code

--- blog/blogs/views.py
+++ blog/blogs/views.py
@@ -90,16 +90,16 @@ def updateProfile(request, username=None):
                 last_name = request.POST['last_name']
                 try:
                     user = User.objects.get(username=username)
-                    if request.FILES['file_field']:
-                        temp_var = True
-                        try:
-                            userprofile = Userprofile.objects.get(user=user)
-                        except Userprofile.DoesNotExist:
-                            temp_var =False
-                            userprofile = Userprofile.objects.create(
-                                profile_pic=request.FILES['file_field'],
-                                user=user
-                            )
+                    # if request.FILES['file_field']:
+                    #     temp_var = True
+                    #     try:
+                    #         userprofile = Userprofile.objects.get(user=user)
+                    #     except Userprofile.DoesNotExist:
+                    #         temp_var =False
+                    #         userprofile = Userprofile.objects.create(
+                    #             profile_pic=request.FILES['file_field'],
+                    #             user=user
+                    #         )
                         # if temp_var:
                         #     userprofile = Userprofile.objects.create(user=user).update(profile_pic=request.FILES['file_field'])
                     loggedUser = request.user
